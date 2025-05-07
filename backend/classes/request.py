1ba30c378c21bac1ed3886448893519d3a608285
anderrow <anderfercar@gmail.com>
2025-05-07 11:08:54 +0200

    Request LotID and PropId are now OOP

--- backend/classes/request.py
+++ backend/classes/request.py
@@ -1,6 +1,44 @@
 from pydantic import BaseModel
+from backend.memory.state import session_data
+from backend.database.query import query_lot_db_id
+from backend.classes.db_connection import DBConnection
+from backend.database.config import config
 
 # Import BaseModel from Pydantic to define the expected structure of the request body
 class PropIdRequest(BaseModel):
     # This field represents the ID of the selected row, expected to be an integer
     propDbId: int
+
+class Request:
+        def __init__(self):        
+            #Get current proportioning id
+            self.current_prop = session_data.get("current_prop_id")
+
+        def return_data(self):
+            raise NotImplementedError("Subclasses should implement this method.")
+
+class RequestPropId(Request):
+    def __init__(self):
+
+        super().__init__()
+
+    def return_data(self):
+        data = self.current_prop
+        return data
+
+class RequestLotId(Request):
+    
+    def __init__(self):
+        
+        super().__init__()
+
+    async def return_data(self):
+        # Initialize the DBConnection object
+        db_connection = DBConnection(config=config) #config is declared in backend/database/config.py
+        #Write the current_prop variable inside the query
+        query = query_lot_db_id.format(current_prop=self.current_prop)
+        #Save lot_id
+        lot_id = await db_connection.fetch_data(query=query)
+        #Extract the lot_id
+        data = lot_id[0]["lot_dbid"]    
+        return data
--- backend/routes/regressor.py
+++ backend/routes/regressor.py
@@ -3,13 +3,11 @@ from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
 from sklearn.linear_model import LinearRegression
 from backend.database.config import config
-from backend.memory.state import session_data
 from backend.classes.db_connection import DBConnection
 from backend.classes.graphs import TraceData, LogScatterPlot
-from backend.database.query import query_regressor_graph, query_lot_db_id
+from backend.database.query import query_regressor_graph
+from backend.classes.request import RequestLotId, RequestPropId
 import numpy as np
-
-
 
 # Create an APIRouter instance
 router = APIRouter(prefix="/regressor")  
@@ -17,30 +15,21 @@ router = APIRouter(prefix="/regressor")
 # Initialize the DBConnection object
 db_connection = DBConnection(config=config) #config is declared in backend/database/config.py
 
-# Example endpoint to check regressor status
 @router.get("/Graph", response_class=HTMLResponse)
 async def generate_graph():
     try:
-        #Get current proportioning id
-        current_prop = session_data.get("current_prop_id")
-        #Write the current_prop variable inside the query
-        query = query_lot_db_id.format(current_prop=current_prop)
-        #Save lot_id
-        lot_id = await db_connection.fetch_data(query=query)
-        #Extract the lot_id from the Json
-        lot_id = lot_id[0]["lot_dbid"] 
+        #Extract lot_id and print it
+        lot_id = await RequestLotId().return_data()
+        print("\n" + "*"*37 + f"\n* LotID: {lot_id} from proportioning {RequestPropId().return_data()} *\n" + "*"*37)
 
-        print("\n" + "*"*35 + f"\n* LotID: {lot_id} from proportioning {current_prop} *\n" + "*"*37) #Debug
+        #Format the query with the current lot id
         query = query_regressor_graph.format(current_lot=lot_id)
-
+        #Generate a dataframe with the DB query
         df = await db_connection.fetch_df(query=query)
 
-        #Generate an empty list for traces
-        trace_list = []
-
+        # Regressions
         df['log_flow'] = np.log10(df['flow'])
 
-        # Regressions
         x_range = np.linspace(df['log_flow'].min(), df['log_flow'].max(), 200)
         flow_range = 10 ** x_range
 
@@ -54,6 +43,8 @@ async def generate_graph():
         coefs_deg3 = np.polyfit(df['log_flow'], df['opening'], 3)
         y_deg3 = np.polyval(coefs_deg3, x_range)
 
+        #Generate an empty list for traces
+        trace_list = []
         #Generate TraceData object and append it
         trace_list.append(TraceData(label="Intermediates", x_data=df['flow'],  y_data=df['opening'], mode="markers", color="blue", marker=dict(size=df['measurement_time'] *3 , color='blue', opacity=0.7))) #Raw Data
         trace_list.append(TraceData(label="Linear Regression", x_data=flow_range,  y_data=y_linear, mode="lines", color="grey", dash="dash")) #Linear Regression
@@ -61,7 +52,7 @@ async def generate_graph():
         trace_list.append(TraceData(label="Polynomial Degree 3", x_data=flow_range,  y_data=y_deg3, mode="lines", color="lime", dash="dash")) #3rd Grade
         
         graph_html = LogScatterPlot(
-            session_data.get("current_prop_id"), 
+            RequestPropId().return_data(), 
             title="", 
             xaxis_title="Flow[kg/s]", 
             yaxis_title="Slide position [mm]", 
@@ -76,4 +67,5 @@ async def generate_graph():
         print(f"Error: {e}")
         # Retrun error
         return HTMLResponse(f"<p>Error generating graph: {e}</p>", status_code=500)
-    
+
+
