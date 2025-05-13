6fe0200089615fd2e39811adc8e076217a5039e1
anderrow <anderfercar@gmail.com>
2025-05-13 16:40:45 +0200

    gitIgnore

--- backend/database/config.py
+++ backend/database/config.py
@@ -8,3 +8,23 @@ config = {
         "Port": "11008"
     }
 }
+config_testrig = {
+    "ConnectionStrings": {
+        "DriverName": "PostgreSQL",
+        "UserID": "amadeus",
+        "Password": "proton",
+        "Database": "amadeus",
+        "Server": "10.5.0.230",
+        "Port": "14008"
+    }
+}
+config_VILO_PROD = {
+    "ConnectionStrings": {
+        "DriverName": "PostgreSQL",
+        "UserID": "amadeus",
+        "Password": "proton",
+        "Database": "amadeus",
+        "Server": "10.5.0.230",
+        "Port": "14008"
+    }
+}
