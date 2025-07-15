e84cca5828b905bb28de862746f0c666747d096f
anderrow <anderfercar@gmail.com>
2025-07-15 14:05:09 +0200

    refactor(common): support per-UUID environment handling based on request

--- backend/routes/common.py
+++ backend/routes/common.py
@@ -3,11 +3,10 @@ from backend.classes.db_connection import DBConnection
 from backend.database.config import config
 from backend.classes.request import RequestPropId
 from backend.database.query import query_valuable_information
+from backend.classes.request import RequestEnvironment
+from backend.database.config import *
 
 router = APIRouter(prefix="/common")  
-
-# Initialize the DBConnection object
-db_connection = DBConnection(config=config) #config is declared in backend/database/config.py   
 
 # ---------- Get Actual PropId to analyze ---------- #
 @router.get("/PropId")
@@ -18,9 +17,12 @@ async def analyzer_status(request: Request):# Get the current proportioning ID f
 # ---------- Extra Information from the PropId ---------- #
 @router.get("/PropIdExtraInfo")
 async def analyzer_status(request: Request):  # Get the current proportioning ID from the request cookies 
+    # Connect to the user's environment
+    db_connection = connect_to_user_environment(request, env_map) 
+
     current_prop = RequestPropId(request).return_data()
 
-    data = await fetch_data(query_valuable_information, current_prop=current_prop)
+    data = await fetch_data(query_valuable_information, current_prop, db_connection)
 
     if data and isinstance(data, list) and isinstance(data[0], dict):
         values = list(data[0].values())  # ['Vitacell R200', 'MIAVIT 174240##Vitacell R200'] (Extract values from dict)
@@ -31,7 +33,7 @@ async def analyzer_status(request: Request):  # Get the current proportioning ID
 
 
 # ---------- Request data for table  ---------- #
-async def fetch_data(query_template: str, current_prop: int) -> dict:
+async def fetch_data(query_template: str, current_prop: int, db_connection: DBConnection) -> dict:
     try:
         #Write the current_prop variable inside the query
         query = query_template.format(current_prop=current_prop)
@@ -43,3 +45,8 @@ async def fetch_data(query_template: str, current_prop: int) -> dict:
         print(f"Error: {str(e)}")
         return {"error": str(e)}
     
+# ------------ Get the current Environment from the request cookies ---------- #
+def connect_to_user_environment(request: Request, env_map):
+    environment = RequestEnvironment(request).return_data()
+    selected_env = env_map.get(environment.upper(), UFA) #Default to UFA if the environment is not found
+    return DBConnection(selected_env)
