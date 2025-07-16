c32a7af64a2baa33a6d25c615e0f77a242f9c919
anderrow <anderfercar@gmail.com>
2025-07-16 11:37:01 +0200

    (Update) Add Route Path  for Settings

--- backend/router.py
+++ backend/router.py
@@ -1,7 +1,7 @@
 # backend/router.py
 from fastapi import APIRouter
 # Import routers from each module in the routes folder
-from backend.routes import proportionings, analyzer, regressor, vms, common
+from backend.routes import proportionings, analyzer, regressor, vms, common, settings
 
 # Create a global APIRouter instance
 router = APIRouter()
@@ -12,4 +12,4 @@ router.include_router(analyzer.router)         #  "Include routes from analyzer 
 router.include_router(common.router)           #  "Include routes from common module"
 router.include_router(regressor.router)        #  "Include routes from regressor module"
 router.include_router(vms.router)              #  "Include routes from vms module"
-
+router.include_router(settings.router)         #  "Include routes from settings module"
